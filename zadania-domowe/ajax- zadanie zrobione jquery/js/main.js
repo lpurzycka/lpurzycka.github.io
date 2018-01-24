ebdf172a7f6c90afa8ff0081e27a2d6af461098c
Lucyna Purzycka <ldamentka@gmail.com>
2018-01-24 21:45:12 +0100

    commit - dwie wersje

--- zadania-domowe/ajax- zadanie zrobione jquery/js/main.js
+++ zadania-domowe/ajax- zadanie zrobione jquery/js/main.js
@@ -1,15 +1,25 @@
 'use strict';
 $(function () {
-
+     
     $("button").click(function () {
         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
             function (pobierzDane) {
-                $("#przycisk").after('<div id="dane-programisty" />') ;   
+//                 
+                $("body").append("<div>");
+                $("div").attr("id", "dane-programisty");
+//            albo:
+//            $("#przycisk").after('<div id="dane-programisty" />') ;  
+
                 $("#dane-programisty").append(
                     "imie: " + pobierzDane.imie + ", " +
                     "nazwisko: " + pobierzDane.nazwisko + ", " +
                     "zawód: " + pobierzDane.zawod + ", " +
                     "firma: " + pobierzDane.firma);
+        
             });
     });
 });
+
+
+
+
